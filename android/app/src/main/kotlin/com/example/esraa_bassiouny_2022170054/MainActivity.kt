b91076ad737f3297dfe246214827ebc4db0c7e2d
esraabassiouny <127542864+esraabassiouny@users.noreply.github.com>
2023-08-23 13:17:48 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/esraa_bassiouny_2022170054/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.esraa_bassiouny_2022170054
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/startingScreen.dart
@@ -0,0 +1,44 @@
+import 'package:esraa_bassiouny_2022170054/homeScreen.dart';
+import 'package:flutter/material.dart';
+
+
+class StartingScreen extends StatefulWidget {
+  const StartingScreen({super.key});
+
+  @override
+  State<StartingScreen> createState() => _StartingScreenState();
+}
+
+class _StartingScreenState extends State<StartingScreen> {
+  @override
+   void initState(){
+     Future.delayed(Duration(seconds: 3),(){
+         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
+             return HomeScreen();
+     }));
+     super.initState();
+   });
+  }
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight= MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.deepOrange,
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Text('talabat',style: TextStyle(
+              color:Colors.white,
+              fontSize: 90,
+              fontWeight: FontWeight.bold,
+            ),),
+          ),
+        ),
+      ),
+    );
+  }
+}
